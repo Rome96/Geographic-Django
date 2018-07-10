3e9b92ab69f9b491912e6360b814ffd899bcec88
Turiano <romero.guzman18@gmail.com>
2018-07-10 09:51:38 -0500

    tipo de relaciones entre modelos

--- continents/models.py
+++ continents/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Continent(models.Model):
+	name = models.CharField(max_length=100)
+	translate = models.CharField(max_length=100)
+	color = models.CharField(max_length=100)
--- countries/models.py
+++ countries/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Country(models.Model):
 	name = models.CharField(max_length=100)
 	code = models.CharField(max_length=5)
+	continent = models.ForeignKey('continents.Continent', on_delete=models.CASCADE)
 
 def __str__(self):
     return self.name
