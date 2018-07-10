b586c6255088189dc5892ff09c5f27735afa34b9
Turiano <romero.guzman18@gmail.com>
2018-07-10 10:07:17 -0500

    creando relaciones uno a uno y muchos a muchos

--- people/models.py
+++ people/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Person(models.Model):
 	first_name = models.CharField(max_length=30)
 	nacionality = models.ManyToManyField('countries.Country') #Relaciones muchos a muchos
+	father = models.OneToOneField('self', on_delete=models.CASCADE, null=True) #Relaciones uno a uno, null=True -> campo no obligatorio
