e8a047bdf3fa56ca64c5621d5d9b96766aec159d
Turiano <romero.guzman18@gmail.com>
2018-07-10 10:00:36 -0500

    Creando modelos de la app people

--- people/models.py
+++ people/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+	first_name = models.CharField(max_length=30)
+	nacionality = models.ManyToManyField('countries.Country') #Relaciones muchos a muchos
