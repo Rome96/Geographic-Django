72919b001994c4cdd10af7e6ebab7d34dbefdb61
Turiano <romero.guzman18@gmail.com>
2018-07-16 12:19:13 -0500

    creacion de las vistas de detalles de la app continents, con sus respectivas url y templates

--- continents/urls.py
+++ continents/urls.py
@@ -1,8 +1,10 @@
+from django.contrib import admin
 from django.urls import path
-from continents.views import ContinentsView
+from continents.views import ContinentsView,ContinentDetailView
 
 app_name = 'continents'
 
 urlpatterns = [
+    path("<int:pk>/", ContinentDetailView.as_view(), name="detail"),
     path("", ContinentsView.as_view(), name="home"),
 ]
--- continents/views.py
+++ continents/views.py
@@ -1,28 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from .models import Continent
+from django.views.generic import TemplateView, ListView, DetailView
 
 # Create your views here.
 
-class ContinentsView(TemplateView):
-	template_name = 'continents/continents.html'
+class ContinentsView(ListView):
+    template_name="continents/home.html"
+    model = Continent
 
-	def get_context_data(self, *args, **kwargs):
-
-		america = {'name': 'américa', 'translation': 'america', 'color': '#F7DC6F'}
-		antartida = {'name': 'antártida', 'translation': 'antartica', 'color': '#2874A6'}
-		europa = {'name': 'europa', 'translation': 'europe', 'color': '#E74C3C'}
-		africa = {'name': 'africa', 'translation': 'africa', 'color': '#2ECC71'}
-		oceania = {'name': 'oceania', 'translation': 'oceania', 'color': '#A93226'}
-		asia = {'name': 'asia', 'translation': 'asia', 'color': '#8E44AD'}
-
-		continents = [
-			america,
-			antartida,
-			europa,
-			africa,
-			oceania,
-			asia
-		]
-
-		return {'continents': continents}
+class ContinentDetailView(DetailView):
+	template_name="continents/detail.html"
+	model = Continent 
