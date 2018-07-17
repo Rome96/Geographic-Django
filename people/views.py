b8fc2406ea828da0a29fd65b4f1bcf55e342eb20
Turiano <romero.guzman18@gmail.com>
2018-07-16 19:32:44 -0500

    validación en el formulario register de la app people

--- people/views.py
+++ people/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from people.forms import RegisterForm
 # Create your views here.
+
 def register(request):
-	form = RegisterForm()
-	if request.method == 'GET':
-		return render(request, "people/register.html", {'form': form})
-	return JsonResponse(request.POST)
+	form = RegisterForm(request.POST or None) # or None para validad que el campo no entre vacio
+	
+	if form.is_valid():
+		return JsonResponse(form.cleaned_data)
+	return render(request, "people/register.html", {'form': form})
+
+		
